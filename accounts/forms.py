cfc680fb5debdc02f51ea0159cba7cf0d421944f
Ahmed Sakib <55790263+omfsakib@users.noreply.github.com>
2021-05-07 00:26:02 +0600

    Add files via upload

---
+++ accounts/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+        )
+    def save(self, commit = True):
+        user = super(RegistrationForm,self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+        return user
+
+class EditProfileForm(UserChangeForm):
+    description=forms.CharField(max_length=100,required=None)
+    city = forms.CharField(max_length=100,required=None)
+    website = forms.URLField(required=None)
+    phone = forms.IntegerField(required=None)
+    image = forms.ImageField(required=None)
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'description',
+            'city',
+            'website',
+            'phone',
+            'image'
+        )
